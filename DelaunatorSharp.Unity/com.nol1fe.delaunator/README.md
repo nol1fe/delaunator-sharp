7ac6d1099292d6a2671d53d1ea65b737e64cb598
nol1fe <patryk.grech@hotmail.com>
2020-05-06 19:37:00 +0200

    Unity Package

---
+++ DelaunatorSharp.Unity/com.nol1fe.delaunator/README.md
@@ -0,0 +1,14 @@
+# Delaunator C#
+
+Fast [Delaunay triangulation](https://en.wikipedia.org/wiki/Delaunay_triangulation) of 2D points implemented in C#.
+
+This code was ported from [Mapbox's Delaunator project](https://github.com/mapbox/delaunator) (JavaScript).
+
+## Documentation
+
+Visit [project repository](https://github.com/nol1fe/delaunator-sharp ) for more information.
+
+
+Points were generated with [Poisson Disc Sampling](https://www.jasondavies.com/poisson-disc)
+implemented by [UniformPoissonDiskSampler](http://theinstructionlimit.com/fast-uniform-poisson-disk-sampling-in-c)
+
---
+++ DelaunatorSharp.Unity/com.nol1fe.delaunator/Runtime/Scripts/DelaunatorExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DelaunatorSharp;
+using System.Linq;
+using System;
+
+
+namespace DelaunatorSharp.Unity.Extensions
+{
+    public static class DelaunatorExtensions
+    {
+        public static IPoint[] ToPoints(this IEnumerable<Vector2> vertices) => vertices.Select(vertex => new Point(vertex.x, vertex.y)).OfType<IPoint>().ToArray();
+        public static IPoint[] ToPoints(this Transform[] vertices) => vertices.Select(x => x.transform.position).OfType<Vector2>().ToPoints();
+
+        public static Vector2[] ToVectors2(this IEnumerable<IPoint> points) => points.Select(point => point.ToVector2()).ToArray();
+        public static Vector3[] ToVectors3(this IEnumerable<IPoint> points) => points.Select(point => point.ToVector3()).ToArray();
+
+        public static Vector2 ToVector2(this IPoint point) => new Vector2((float)point.X, (float)point.Y);
+        public static Vector3 ToVector3(this IPoint point) => new Vector3((float)point.X, (float)point.Y);
+    }
+}
