c139480c5b962839f771fe67a20fcc74727b17f9
Patryk Grech <patryk.grech@hotmail.com>
2021-09-01 17:50:13 +0200

    Add GetVoronoiCellsBenchmark
    Add simple benchmark of GetVoronoiCells

---
+++ DelaunatorSharp.Benchmark/GetVoronoiCellsBenchmark.cs
@@ -0,0 +1,37 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DelaunatorSharp.Benchmark
+{
+    [SimpleJob(RunStrategy.ColdStart, warmupCount:10, targetCount: 10)]
+    [HtmlExporter]
+    public class GetVoronoiCellsBenchmark
+    {
+        private Distribution distribution = new Distribution();
+        private IPoint[] points;
+
+        [Params(5000)]
+        public int Count;
+
+        [ParamsAllValues]
+        public Distribution.Type Type { get; set; }
+
+        private Delaunator delaunator;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            points = distribution.GetPoints(Type, Count).ToArray();
+            delaunator = new Delaunator(points);
+        }
+
+        [Benchmark]
+        public void GetVoronoiCells() 
+        {
+            using var enumerator = delaunator.GetVoronoiCells().GetEnumerator();
+            while (enumerator.MoveNext()) { }
+        }
+    }
+}
